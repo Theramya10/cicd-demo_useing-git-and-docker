9ecb091a06a73850bc2e387f91dcd945ad3c95de
Ramya Ranjan Sahoo <114637302+Theramya10@users.noreply.github.com>
2024-09-27 10:02:01 +0530

    Update1.2 app.py
    cicd demo

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-   return "Wellcome to theramya10 World!"
+   return "Wellcome to theramya10_World!"
 
 if __name__ == "__main__":
    app.run(host='0.0.0.0', port=8080)
